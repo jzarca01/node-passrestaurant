ccb199de3ac50f042bb078aa8b20cd5e1be4296b
Jérémie Zarca <jeremie.zarca@gmail.com>
2018-03-12 23:49:15 +0100

    figured out crypto HmacSHA512, added getConsumerInfo

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const axios = require("axios");
 const base64 = require("base-64");
 
+const { encrypt } = require('./lib/utils');
+
 const config = require("./config.json")
 
-const BASE_URL = "https://api-moncompte.sodexopass.fr/v2.2";
+const BASE_URL = `https://api-moncompte.sodexopass.fr/${config.WS_VERSION}`;
 const AUTH_URL = BASE_URL + "/token";
+
+const CONSUMER_URL = BASE_URL + `/${config.CLIENT_ID}/account`
 
 function sodexoApi() {
 }
@@ -25,7 +29,33 @@ sodexoApi.prototype.signIn = async function(login, password) {
             },
             responseType: 'json'
         });
-        console.log('response', response.data)
+        return response.data;
+    }
+    catch(error) {
+        console.log("error", error);
+    }
+}
+
+sodexoApi.prototype.getConsumerInfo = async function(login, accessToken) {
+    try {
+        axios.interceptors.request.use(function (axiosConfig) {
+            const timestamp = Math.floor(Date.now()/1000).toString()
+            axiosConfig.params = {
+                timestamp: timestamp,
+                signature: encrypt(axiosConfig.url+`?timestamp=${timestamp}`, config.CLIENT_KEY)
+            }
+            return axiosConfig;
+          }, function (error) {
+            return Promise.reject(error);
+          });
+        let response = await axios({
+            method: "GET",
+            url: CONSUMER_URL + `/${login}/consumer`,
+            headers: {
+                "Authorization": "Bearer " + accessToken
+            },
+            responseType: 'json'
+        });
         return response.data;
     }
     catch(error) {
